d378b154cf0bb540fc97fa88b3dd264ae34a549f
yash-dani <daniyash19@gmail.com>
2020-08-04 08:17:47 -0400

    added favicon, improved captcha

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def is_human(captcha_response):
         Returns True captcha test passed for the submitted form 
         else returns False.
     """
-    secret = ""
+    secret = ''
     payload = {'response':captcha_response, 'secret':secret}
     response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
     response_text = json.loads(response.text)
