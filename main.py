7cd6667e7f43d58ac556d672b415013a17269059
yash-dani <daniyash19@gmail.com>
2020-08-04 07:54:18 -0400

    added captcha + margin improvements

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template
 import os
 app = Flask(__name__)
 import gpt3
+import requests
+import json
 
 @app.route('/index')
 def index():
@@ -15,15 +17,29 @@ def make():
 
 @app.route('/', methods=['post', 'get'])
 def generate():
+    sitekey = "6LcaSboZAAAAACK98x__otD9iW_7KXhSdAXYmT_H"
     message = ''
     if request.method == 'POST':
            if message == '':
-               message = gpt3.getGPT3(request.form['abstract'].replace("A:", "").replace("Here this is, made for a 2nd grader:",""))
+              captcha_response = request.form['g-recaptcha-response']
+              if is_human(captcha_response):
+                message = gpt3.getGPT3(request.form['abstract'].replace("A:", "").replace("Here this is, made for a 2nd grader:",""))
            else:
             message = ''
         #print(message)
         
-    return render_template('generate.html', message=message)
+    return render_template('generate.html', message=message, sitekey=sitekey)
+
+def is_human(captcha_response):
+    """ Validating recaptcha response from google server.
+        Returns True captcha test passed for the submitted form 
+        else returns False.
+    """
+    secret = "6LcaSboZAAAAAP43wF3xt3k-D6Bt1WEkrnI0K05D"
+    payload = {'response':captcha_response, 'secret':secret}
+    response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
+    response_text = json.loads(response.text)
+    return response_text['success']
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
