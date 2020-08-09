0eb791cdafe6ae396d44f92498e84857c8e6bcdb
yash-dani <daniyash19@gmail.com>
2020-08-08 23:30:59 -0400

    added laoding screen

--- static/process.js
+++ static/process.js
@@ -1,14 +1,3 @@
 function count_up(obj) {
 	document.getElementById('count1').innerHTML = obj.value.length + '/2000';
 }	
-$(document).ready(function() {
-    var text_max = 99;
-    $('#textarea_feedback').html(text_max + ' characters remaining');
-
-    $('#textarea').keyup(function() {
-        var text_length = $('#textarea').val().length;
-        var text_remaining = text_max - text_length;
-
-        $('#textarea_feedback').html(text_remaining + ' characters remaining');
-    });
-});
